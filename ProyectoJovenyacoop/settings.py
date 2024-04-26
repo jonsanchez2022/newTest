9c2d1f0cebe9ee8e1e532f70a5d561b0066fbf20
jonsanchez2022 <122843312+jonsanchez2022@users.noreply.github.com>
2024-04-25 20:10:57 -0400

    dbjarto2

--- ProyectoJovenyacoop/settings.py
+++ ProyectoJovenyacoop/settings.py
@@ -88,15 +88,14 @@ WSGI_APPLICATION = 'ProyectoJovenyacoop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'defensores',
         'USER': 'postgres',
         'PASSWORD': 'defensores123',
-        'HOST': '127.0.0.1',
+        'HOST': 'localhost',
         'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
